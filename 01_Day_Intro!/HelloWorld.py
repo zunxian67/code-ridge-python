f4502a826eae40413a14dd943ab6c01c37ae2db7
zunxian67 <zunxiankuang@gmail.com>
2026-08-20 08:18:10 -0400

    Update HelloWorld.py

--- 01_Day_Intro!/HelloWorld.py
+++ 01_Day_Intro!/HelloWorld.py
@@ -1,4 +1,20 @@
 '''
 Welcome to Code Ridge Python
 
+Python is a popular computer programming language where most people start learning
+their first text-based progamming language. It used for general-purposes such as 
+pygame, python guis, python data-analytics such as mpl, pandas, and numpy.
+One of the newest things that python is useful for is AI coding using cv2,
+torch, pyaudio, cobra etc.
+
+Lets learn our first command in python!
 '''
+print("Hello World")
+
+'''
+
+This sends a command called print()
+The print() command sends "Hello World" to the console and displays it.
+
+''''
+
