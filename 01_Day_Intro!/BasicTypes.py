0d16558c3790455163194e055975ae2018bf9447
zunxian67 <zunxiankuang@gmail.com>
2026-08-20 08:56:43 -0400

    Create BasicTypes.py

---
+++ 01_Day_Intro!/BasicTypes.py
@@ -0,0 +1,40 @@
+'''
+Welcome to the types of python
+
+'''
+print(type(10))# Int
+'''
+Integers are whole numbers
+'''
+print(type(3.14))# Float
+'''
+Floats are numbers with decimals
+
+'''
+
+print(type(True))#Boolean
+
+'''
+Booleans are like levers
+They flick on or off
+True or False.
+
+'''
+print(type("Hello World"))#String
+
+'''
+This is a String
+A String holds a line of text
+'''
+char CodeRidge = 'A';# THIS WILL CRASH
+'''
+Python doesn't have a built in char type.
+This is an example of how programming languages are different.
+'''
+print(type([1, 2, 3]))# List
+'''
+We will get into this more later since this is more complex
+But all you need to know is that it can store stuff like a list
+'''
+
+
