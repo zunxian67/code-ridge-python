83877f4b4587938b27cc9bdd5fa7ae9a26be3e41
zunxian67 <zunxiankuang@gmail.com>
2026-08-20 08:42:54 -0400

    Create MathFunctions.py

---
+++ 01_Day_Intro!/MathFunctions.py
@@ -0,0 +1,43 @@
+'''
+Welcome to the second file,
+Here you will learn math functions built into python.
+
+'''
+print(2+3)
+
+'''
+The plus sign indicates that you want to add two numbers together
+In this case 2+3, which results in 5
+'''
+
+print(3 - 1)  
+'''
+The minus sign indicates that you want to add subtraction numbers together
+In this case 3-1, which results in 2
+'''
+
+print(2 * 3)   # multiplication(*)
+print(3 / 2)   # division(/)
+'''
+Python follows the same order as PEMDAS
+Where Multiplication and Divison is always first
+
+'''
+
+print(3 ** 2)  # exponential(**)
+print(3 % 2)   # modulus(%)
+
+'''
+Here are some special functions
+Exponential and Parentheses will go first.
+Modulus function that returns an integer based on the remainder.
+Ex: 3 % 2, since 3 divided by 2 is remainder of 1.
+This would print 1
+Modulus has the order of divison.
+'''
+print(3 // 2)  # Floor division operator(//)
+
+'''
+uhh look up what floor does
+
+'''
